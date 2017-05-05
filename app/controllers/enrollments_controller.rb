626f43ff6d31b0b7f517d5bc26e041eaa22bbf6a
Enos Vazquez <enosvazquez@gmail.com>
2017-05-05 00:10:20 +0000

    Setup links in top nav

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,37 +1,35 @@
 class EnrollmentsController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def create
-        if current_course.premium?
-            current_user.enrollments.create(course: current_course)
+  def create
+    if current_course.premium?
 
-            # Amount in cents
-            @amount = (current_course.cost * 100).to_i
+      # Amount in cents
+      @amount = (current_course.cost * 100).to_i
 
-            customer = Stripe::Customer.create(
-              email: params[:stripeEmail],
-              source: params[:stripeToken]
-            )
+      customer = Stripe::Customer.create(
+        email: params[:stripeEmail],
+        source: params[:stripeToken]
+      )
 
-            charge = Stripe::Charge.create(
-              customer: customer.id,
-              amount: @amount,
-              description: 'Flixter Premo Content',
-              currency: 'usd'
-            )
-
-            current_user.enrollments.create(course: current_course)
-            redirect_to course_path(current_course)
-            rescue Stripe::CardError => e
-            flash[:error] = e.message
-            redirect_to root_path
-        end
+      charge = Stripe::Charge.create(
+        customer: customer.id,
+        amount: @amount,
+        description: 'Flixter Premo Content',
+        currency: 'usd'
+      )
     end
 
-    private
+    current_user.enrollments.create(:course => current_course)
+    redirect_to course_path(current_course)
+    rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to root_path
+  end
 
-    def current_course
-        @current_course ||= Course.find(params[:course_id])
-    end
+  private
 
+  def current_course
+  @current_course ||= Course.find(params[:course_id])
+  end
 end
