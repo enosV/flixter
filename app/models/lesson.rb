0ce65f714e4b66fdd26806c9db108e16cb716af5
Enos Vazquez <enosvazquez@gmail.com>
2017-05-06 21:28:48 +0000

    New lesson form modal

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,7 +3,7 @@ class Lesson < ApplicationRecord
     mount_uploader :video, VideoUploader
 
     include RankedModel
-    ranks :row_order, with_same: :section_id
+    ranks :row_order, :with_same => :section_id
 
     def next_lesson
         lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
@@ -11,6 +11,6 @@ class Lesson < ApplicationRecord
           return section.next_section.lessons.rank(:row_order).first
         end
         
-        return lessons
+        return lesson
     end
 end
